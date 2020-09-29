ac4a0d3353b42c66878ac597656b19cde3391167
Yifei <valkriaine@outlook.com>
2020-09-28 18:31:20 -0700

    Setup bottom drawer, get current location of the user

--- app/src/main/java/com/digitalsmart/mutify/MapsActivity.java
+++ app/src/main/java/com/digitalsmart/mutify/MapsActivity.java
@@ -1,5 +1,14 @@
 package com.digitalsmart.mutify;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -7,42 +16,93 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import no.danielzeller.blurbehindlib.BlurBehindLayout;
 
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private Location currentLocation;
+    private Marker marker;
+    private static final int FINE_LOCATION_CODE = 15;
+    private static final int COARSE_LOCATION_CODE = 16;
+    private static final int INTERNET_CODE = 17;
+
+
+
+    //check and request permission
+    private void checkPermission(String permission, int requestCode)
+    {
+        if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED)
+        {
+            ActivityCompat.requestPermissions(this, new String[] { permission }, requestCode);
+        }
+    }
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
+        //check and request location permissions
+        checkPermission(Manifest.permission.ACCESS_FINE_LOCATION, FINE_LOCATION_CODE);
+        checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION, COARSE_LOCATION_CODE);
+        checkPermission(Manifest.permission.INTERNET, INTERNET_CODE);
+
+
+
         setContentView(R.layout.activity_maps);
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
-        BlurBehindLayout blurLayout = findViewById(R.id.blurLayout);
+
+
+        //setup blur for search bar
+        BlurBehindLayout blurLayout = findViewById(R.id.searchbar);
         blurLayout.setViewBehind(findViewById(R.id.map));
+
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
+
     }
+
+
+
+
 
 
     @Override
-    protected void onStart() {
-        super.onStart();
+    public void onLocationChanged(@NonNull Location location)
+    {
+        currentLocation = location;
+        LatLng current = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        if (marker != null)
+        {
+            //refresh marker
+            marker.remove();
+            marker = mMap.addMarker(new MarkerOptions().position(current).title("You are here."));
+        }
+        else {
+            //initialize marker and zoom in the camera
+            marker = mMap.addMarker(new MarkerOptions().position(current).title("You are here."));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 15));
+        }
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -50,10 +110,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
