9afe82fe7a66b77a41c717ab8a8097e6da0c4644
Yifei <valkriaine@outlook.com>
2020-10-10 19:17:25 -0700

    Implemented Places search

--- app/src/main/java/com/digitalsmart/mutify/MapsActivity.java
+++ app/src/main/java/com/digitalsmart/mutify/MapsActivity.java
@@ -6,6 +6,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
 import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,19 +19,24 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.digitalsmart.mutify.util.BlurController;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.*;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 import com.valkriaine.factor.HomePager;
 import no.danielzeller.blurbehindlib.BlurBehindLayout;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -103,6 +109,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         //initialize fused location provider
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+        //must call these methods when implementing background service
+        //remove these method calls if Mutify does not need to constantly update the user's location
         initializeLocationCallBack();
         createLocationRequest();
         startLocationUpdates();
@@ -110,14 +120,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         //retrieve user's current location at app launch
         getCurrentLocation(null);
-
     }
 
 
 
 
-
-    //these methods are required for FusedLocationProvider to work
+    //call these methods to start constantly updating the user's location info
+    //as long as the app is still running (in the foreground or minimized)
+    //app will stop updating location info if the user manually closes the app
+    //this is not the same as background service, implement background service separately
     //*********************************************************************************************************************
     protected void createLocationRequest()
     {
@@ -128,9 +139,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
     protected void startLocationUpdates()
     {
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest,
-                locationCallback,
-                Looper.getMainLooper());
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
     }
     protected void initializeLocationCallBack()
     {
@@ -153,23 +162,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
 
-
-
     //button click events
     //*********************************************************************************************************************
     //call this method to manually get the user's current location
     public void getCurrentLocation(View view)
     {
         if (fusedLocationProviderClient != null)
-            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    // Got last known location. In some rare situations this can be null.
-                    if (location != null) {
-                        currentLocation = location;
-                        currentLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-                        map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15));
-                    }
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, location -> {
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    currentLocation = location;
+                    currentLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15));
                 }
             });
     }
@@ -211,8 +215,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         userDataManager.add(markerUserLocation);
         homePager.setCurrentItem(1, true);
     }
-
-
 
 
 
@@ -307,12 +309,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
 
-
     //add other methods here
     //*********************************************************************************************************************
-    //link layout components, find views by id
+    //link layout components and initialize places sdk, find views by id
     private void initializeComponents()
     {
+        // Initialize the SDK
+        Places.initialize(getApplicationContext(), this.getResources().getString(R.string.maps_api_key));
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+        assert autocompleteFragment != null;
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.LAT_LNG, Place.Field.ID, Place.Field.NAME));
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener()
+        {
+            @Override
+            public void onPlaceSelected(@NotNull Place place)
+            {
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 15));
+            }
+
+            @Override
+            public void onError(@NotNull Status status)
+            {
+                Log.i("places_error", "An error occurred: " + status);
+            }
+        });
+
+
+
+        //initialize views
         drawer = findViewById(R.id.drawer);
         homePager = findViewById(R.id.home_pager);
         homePager.addView(findViewById(R.id.add_page), 0);
@@ -321,46 +345,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         ImageView marker = findViewById(R.id.marker_sprite);
         CardView addTile = findViewById(R.id.add_tile);
         CardView menuTile = findViewById(R.id.menu_tile);
+
+
+        //set up blur effect and transition animations
         BlurController blurController = new BlurController(findViewById(R.id.background), blurBackground, addTile, menuTile, homePager);
         drawer.addPanelSlideListener(blurController);
         homePager.addOnPageChangeListener(blurController);
+
+
+        //spring animations for map marker
         dragSpring = new SpringAnimation(marker, DynamicAnimation.TRANSLATION_Y, -30);
         settleSpring = new SpringAnimation(marker, DynamicAnimation.TRANSLATION_Y, 0);
     }
     //enable the app to retrieve the marker's location
     private void configureCamera()
     {
-        GoogleMap.OnCameraIdleListener onCameraIdleListener = new GoogleMap.OnCameraIdleListener() {
-            @Override
-            public void onCameraIdle()
-            {
-                dragSpring.skipToEnd();
-                settleSpring.start();
-                LatLng latLng = map.getCameraPosition().target;
-                Geocoder geocoder = new Geocoder(MapsActivity.this);
-                markerLocation = new Location("Camera Location");
-                markerLocation.setLatitude(latLng.latitude);
-                markerLocation.setLongitude(latLng.longitude);
-                List<Address> addressList = null;
-                try {
-                    addressList = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                if (addressList != null && addressList.size() > 0) {
-                    markerUserLocation = new UserLocation("Marker Location", addressList);
-                }
+        GoogleMap.OnCameraIdleListener onCameraIdleListener = () -> {
+            dragSpring.skipToEnd();
+            settleSpring.start();
+            LatLng latLng = map.getCameraPosition().target;
+            Geocoder geocoder = new Geocoder(MapsActivity.this);
+            markerLocation = new Location("Camera Location");
+            markerLocation.setLatitude(latLng.latitude);
+            markerLocation.setLongitude(latLng.longitude);
+            List<Address> addressList = null;
+            try {
+                addressList = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (addressList != null && addressList.size() > 0) {
+                markerUserLocation = new UserLocation("Marker Location", addressList);
             }
         };
 
-        GoogleMap.OnCameraMoveStartedListener onCameraMoveStartedListener = new GoogleMap.OnCameraMoveStartedListener()
-        {
-            @Override
-            public void onCameraMoveStarted(int i)
-            {
-                dragSpring.start();
-            }
-        };
+        GoogleMap.OnCameraMoveStartedListener onCameraMoveStartedListener = i -> dragSpring.start();
         map.setOnCameraIdleListener(onCameraIdleListener);
         map.setOnCameraMoveStartedListener(onCameraMoveStartedListener);
     }
